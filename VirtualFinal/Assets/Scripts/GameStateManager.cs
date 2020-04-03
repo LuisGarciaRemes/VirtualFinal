5335c0842156b18bf4caddd9edee845593576706
Luis Garcia Remes <luisagarciaremes@hotmail.com>
2020-04-02 22:45:21 -0600

    Init push. Splitscreen working

---
+++ VirtualFinal/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameStateManager : MonoBehaviour
+{
+    private static GameStateManager m_instance;
+    [SerializeField] private GameObject[] m_initSpawnPoints;
+    private int m_numPlayers = 0;
+    [SerializeField] private PlayerInputManager m_inputManager;
+
+    public static GameStateManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = new GameObject("GameStateManager").AddComponent<GameStateManager>();
+            }
+            return m_instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+        }
+        else if (m_instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public int AddPlayer()
+    {
+        m_numPlayers++;
+
+        return m_numPlayers;
+    }
+
+    public Vector3 GetInitialSpawnPoint(int i_playerID)
+    {
+        return m_initSpawnPoints[i_playerID-1].transform.position;
+    }
+
+    public void SetSplitScreen(bool i_bool)
+    {
+        m_inputManager.splitScreen = i_bool;
+    }
+}
---
+++ VirtualFinal/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private int m_playerID;
+    internal Vector3 m_spawnPoint;
+    private Vector3 m_velocity;
+    private Rigidbody m_rb;
+    private float m_speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerID = GameStateManager.instance.AddPlayer();
+        transform.position = GameStateManager.instance.GetInitialSpawnPoint(m_playerID);
+        gameObject.GetComponent<PlayerInput>().camera = GameObject.Find("Player"+m_playerID+"Camera").GetComponent<Camera>();
+        Debug.Log("Created Player " + m_playerID);
+        m_rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rb.velocity = m_velocity*m_speed;
+    }
+
+    private void OnMove(InputValue value)
+    {
+       m_velocity.x = value.Get<Vector2>().x;
+        m_velocity.z = value.Get<Vector2>().y;
+        m_velocity.y = 0.0f;
+    }
+
+    private void OnAButton()
+    {
+        GameStateManager.instance.SetSplitScreen(true);
+    }
+}
