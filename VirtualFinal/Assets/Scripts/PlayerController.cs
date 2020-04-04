4266d7267ef50139941aef49b4f1280a25737bbc
Luis Garcia Remes <luisagarciaremes@hotmail.com>
2020-04-04 14:39:15 -0600

    Added sword and block mechanics

--- VirtualFinal/Assets/Scripts/PlayerController.cs
+++ VirtualFinal/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,19 @@ public class PlayerController : MonoBehaviour
     private Vector3 m_velocity;
     private Rigidbody m_rb;
     private float m_speed = 10;
+    [SerializeField] GameObject m_RightHand;
+    [SerializeField] GameObject m_LeftHand;
+    private bool m_swingsword;
+    private float m_swingRot;
+    private float m_originalSwordRot;
+    [SerializeField] private float m_finalSwordRot;
+    private bool m_holdshield;
+    private float m_shieldRot;
+    private float m_originalshieldRot;
+    [SerializeField] private float m_finalShieldRot;
+
+    [SerializeField] private float m_swordSpeed = 20.0f;
+    [SerializeField] private float m_shieldSpeed = 20.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +33,28 @@ public class PlayerController : MonoBehaviour
         gameObject.GetComponent<PlayerInput>().camera = GameObject.Find("Player"+m_playerID+"Camera").GetComponent<Camera>();
         Debug.Log("Created Player " + m_playerID);
         m_rb = gameObject.GetComponent<Rigidbody>();
+        m_swingsword = false;
+        m_originalSwordRot = m_RightHand.transform.rotation.eulerAngles.y;
+        m_swingRot = m_originalSwordRot;
+        m_originalshieldRot = m_LeftHand.transform.rotation.eulerAngles.y;
+        m_shieldRot = m_originalshieldRot;
+        m_holdshield = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         m_rb.velocity = m_velocity*m_speed;
+
+        if(m_swingsword)
+        {
+            SwingSword();
+        }
+
+        if (m_holdshield)
+        {
+            HoldShield();
+        }
     }
 
     private void OnMove(InputValue value)
@@ -35,8 +64,93 @@ public class PlayerController : MonoBehaviour
         m_velocity.y = 0.0f;
     }
 
+    private void OnStartButton()
+    {
+        Debug.Log("Start");
+        GameStateManager.instance.SetSplitScreen(true);
+    }
+
+    private void OnSelectButton()
+    {
+        Debug.Log("Select");
+    }
+
     private void OnAButton()
     {
-        GameStateManager.instance.SetSplitScreen(true);
+        Debug.Log("A");
+    }
+
+    private void OnBButton()
+    {
+        Debug.Log("B");
+    }
+
+    private void OnYButton()
+    {
+        Debug.Log("Y");
+    }
+
+    private void OnXButton()
+    {
+        Debug.Log("X");
+    }
+
+    private void OnRightTrigger()
+    {
+        Debug.Log("Right Trigger");
+        if (!m_swingsword)
+        {
+            m_swingsword = true;
+            m_swingRot = m_finalSwordRot;
+        }
+
+        if(m_holdshield)
+        {
+            OnLeftTriggerRelease();
+        }
+    }
+
+    private void OnLeftTrigger()
+    {
+        Debug.Log("Left Trigger Pressed");
+        if (!m_holdshield)
+        {
+            m_holdshield = true;
+            m_shieldRot = m_finalShieldRot;
+        }
+    }
+
+    private void OnLeftTriggerRelease()
+    {
+        Debug.Log("Left Trigger Release");
+        if (m_holdshield)
+        {
+            m_shieldRot = m_originalshieldRot;
+        }
+    }
+
+    private void SwingSword()
+    {
+        m_RightHand.transform.rotation = Quaternion.Lerp(m_RightHand.transform.rotation, Quaternion.Euler(m_RightHand.transform.rotation.eulerAngles.x,m_swingRot, m_RightHand.transform.rotation.eulerAngles.z),Time.deltaTime * m_swordSpeed);
+
+        if (((m_RightHand.transform.rotation.eulerAngles.y <= m_originalSwordRot && m_RightHand.transform.rotation.eulerAngles.y >= m_originalSwordRot - 1) && m_swingRot == m_originalSwordRot))
+        {
+            m_swingsword = false;
+        }
+
+        if(((m_RightHand.transform.rotation.eulerAngles.y <= m_finalSwordRot + 361 && m_RightHand.transform.rotation.eulerAngles.y >= m_finalSwordRot + 360) && m_swingRot == m_finalSwordRot))
+        {
+            m_swingRot = m_originalSwordRot;
+        }
+    }
+
+    private void HoldShield()
+    {
+        m_LeftHand.transform.rotation = Quaternion.Lerp(m_LeftHand.transform.rotation, Quaternion.Euler(m_LeftHand.transform.rotation.eulerAngles.x, m_shieldRot, m_LeftHand.transform.rotation.eulerAngles.z), Time.deltaTime * m_shieldSpeed);
+
+        if (((m_LeftHand.transform.rotation.eulerAngles.y <= m_originalshieldRot + 1 && m_LeftHand.transform.rotation.eulerAngles.y >= m_originalshieldRot) && m_shieldRot == m_originalshieldRot))
+        {
+            m_holdshield = false;
+        }
     }
 }
