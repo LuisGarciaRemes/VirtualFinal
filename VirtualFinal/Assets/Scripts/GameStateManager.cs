767b0228feb1a77130019c2a4c99633c9baf39c8
Luis Garcia Remes <luisagarciaremes@hotmail.com>
2020-05-06 00:05:24 -0600

    ef

--- VirtualFinal/Assets/Scripts/GameStateManager.cs
+++ VirtualFinal/Assets/Scripts/GameStateManager.cs
@@ -157,7 +157,6 @@ public class GameStateManager : MonoBehaviour
                 playerPanels[winningPlayer - 1].SetActive(true);
                 playerPanels[winningPlayer - 1].GetComponentInChildren<Text>().text = "Player " + winningPlayer + " Wins!";
                 playerHUDs[winningPlayer - 1].SetActive(false);
-                Time.timeScale = 0.0f;
             }
     }
 
